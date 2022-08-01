57b5451172aa316d8bf00efef98436e6dc559c89
AhmedEissa2021 <88287282+AhmedEissa2021@users.noreply.github.com>
2022-08-01 14:43:30 +0200

    Add files via upload

---
+++ bsas_account_invoice/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Bsas Account Invoice",
+
+    'category': 'Accounting',
+    'version': '15.0',
+    'depends': ['base','account','account_accountant'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/account_move.xml',
+        'views/templates.xml',
+        'report/invoice_report.xml',
+    ],
+    'license': 'LGPL-3',
+
+}
