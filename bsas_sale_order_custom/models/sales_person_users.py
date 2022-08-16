2f2bf30751a5fc1b58aa86f2edd6a0e9edec2f43
AhmedEissa2021 <88287282+AhmedEissa2021@users.noreply.github.com>
2022-08-16 12:01:29 +0200

    Delete bsas_sale_order_custom directory

--- bsas_sale_order_custom/models/sales_person_users.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-class SalesPersonUsers(models.Model):
-    _name = 'sales.person.users'
-    _rec_name = 'name'
-    _description = 'Sales Person Users'
-
-    name = fields.Char()
-
