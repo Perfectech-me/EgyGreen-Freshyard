277ea630d878bf129c3790a52eb3678ca56ad6b2
AhmedEissa2021 <88287282+AhmedEissa2021@users.noreply.github.com>
2022-08-16 12:17:06 +0200

    Add files via upload

---
+++ bsas_sale_order_custom/models/purchase_order.py
@@ -0,0 +1,18 @@
+from odoo import models, fields, api
+class PurchaseOrderInherit(models.Model):
+    _inherit = 'purchase.order'
+
+    order_category = fields.Selection(string="Order Catrgory", selection=[
+        ('International', 'International'),
+        ('Local', 'Local'),
+    ], default='International')
+
+    export_type = fields.Selection(string="Export Type", selection=[('fresh', 'fresh'), ('frozen', 'frozen'),
+                                                                    ('food_products', 'Food Products'), ],
+                                   default='fresh')
+    product_type = fields.Selection(string="Product Type", selection=[
+        ('row_materials', 'Row Materials'), ('sort', 'Sort'), ('packing', 'Packing'),
+        ('finish_products', 'Finish Products'), ('other', 'Other')], default='row_materials')
+
+
+
