e0e7d9319636b17cf54ee19bf69ed748032e3e49
AhmedEissa2021 <88287282+AhmedEissa2021@users.noreply.github.com>
2022-08-16 12:10:53 +0200

    Delete bsas_sale_order_custom directory

--- bsas_sale_order_custom/models/account_move.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-class AccountMoveInherit(models.Model):
-    _inherit = 'account.move'
-
-    invoice_person_user_id = fields.Many2one(comodel_name="sales.person.users", string="Sales Person")
-    bank_ids = fields.Many2many(comodel_name="res.partner.bank",string="Bank Accounts")
-
-
--- bsas_sale_order_custom/wizard/__init__.py
+++
@@ -1,3 +0,0 @@
-
-from . import approve_credit_limit
-from . import refuse_credit_limit
