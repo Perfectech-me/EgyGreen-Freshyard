3e333cd4a8f792358d865c68766cc905fd70a6d3
AhmedEissa2021 <88287282+AhmedEissa2021@users.noreply.github.com>
2022-08-17 23:13:02 +0200

    Delete bsas_sale_order_custom_extention directory

--- bsas_sale_order_custom_extention/models/customer_type.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-class ResPArtnerTypes(models.Model):
-    _name = 'res.partner.types'
-    _rec_name = 'name'
-    _description = 'customer Type'
-
-    name = fields.Char()
-
