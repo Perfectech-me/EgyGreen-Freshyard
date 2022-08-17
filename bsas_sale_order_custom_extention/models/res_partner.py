dde298dc0b2c8ed5cb8ba48ea86d9a31d2ff8dec
AhmedEissa2021 <88287282+AhmedEissa2021@users.noreply.github.com>
2022-08-17 23:00:05 +0200

    Add files via upload

--- bsas_sale_order_custom_extention/models/res_partner.py
+++ bsas_sale_order_custom_extention/models/res_partner.py
@@ -7,7 +7,7 @@ class ResPartnerInherit(models.Model):
     insurance_type = fields.Selection(string="", selection=[('yes', 'Yes'), ('no', 'No'), ],)
     insurance_credit_limit = fields.Float(string="Insurance Credit Limit")
     insurance_currency_id = fields.Many2one(comodel_name="res.currency")
-    insurance_company = fields.Char(string="Insurance Company")
+    insurance_company_text = fields.Char(string="Insurance Company")
     continent = fields.Selection(string="Continent", selection=[('Africa', 'Africa'), ('Antarctica', 'Antarctica'),
                                                    ('Asia', 'Asia'),('Australia', 'Australia'),('Europe', 'Europe'),
                                                    ('North_America', 'North America'),('South_America', 'South America')
@@ -18,7 +18,7 @@ class ResPartnerInherit(models.Model):
     def _set_insurance_credit_limit(self):
         self.insurance_credit_limit=0.0
         self.insurance_currency_id=False
-        self.insurance_company=""
+        self.insurance_company_text=""
 
     def _get_name(self):
         res=super(ResPartnerInherit, self)._get_name()
