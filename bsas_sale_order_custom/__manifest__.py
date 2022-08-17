d12b8f7b79f252c45ba5442a2c8906dd05fb8fb7
AhmedEissa2021 <88287282+AhmedEissa2021@users.noreply.github.com>
2022-08-17 14:24:09 +0200

    Add files via upload

--- bsas_sale_order_custom/__manifest__.py
+++ bsas_sale_order_custom/__manifest__.py
@@ -16,7 +16,7 @@
         'views/sales_person_users.xml',
         'views/account_move.xml',
         'views/res_partner.xml',
-        'views/customer_type.xml',
+        # 'views/customer_type.xml',
         'wizard/approve_credit_limit.xml',
         'wizard/refuse_credit_limit.xml',
     ],
--- bsas_sale_order_custom/models/partner.py
+++ bsas_sale_order_custom/models/partner.py
@@ -13,11 +13,11 @@ class ResPartnerInherit(models.Model):
         default='contact',
         help="Invoice & Delivery addresses are used in sales orders. Private addresses are only visible by authorized users.")
 
-    customer_type_id = fields.Many2one(comodel_name="res.partner.type", string="Customer Type")
+    # customer_type_id = fields.Many2one(comodel_name="res.partner.type", string="Customer Type")
     insurance_type = fields.Selection(string="", selection=[('yes', 'Yes'), ('no', 'No'), ],)
     insurance_credit_limit = fields.Float(string="Insurance Credit Limit")
     insurance_currency_id = fields.Many2one(comodel_name="res.currency")
-    insurance_company = fields.Char(string="Insurance Company")
+    # insurance_company = fields.Char(string="Insurance Company")
     continent = fields.Selection(string="Continent", selection=[('Africa', 'Africa'), ('Antarctica', 'Antarctica'),
                                                    ('Asia', 'Asia'),('Australia', 'Australia'),('Europe', 'Europe'),
                                                    ('North_America', 'North America'),('South_America', 'South America')
@@ -28,10 +28,10 @@ class ResPartnerInherit(models.Model):
     def _set_insurance_credit_limit(self):
         self.insurance_credit_limit=0.0
         self.insurance_currency_id=False
-        self.insurance_company=""
+        # self.insurance_company=""
 
-    def _get_name(self):
-        res=super(ResPartnerInherit, self)._get_name()
-        if self.continent:
-            res = "%s \n %s" % (res, self.continent)
-        return res
+    # def _get_name(self):
+    #     res=super(ResPartnerInherit, self)._get_name()
+    #     if self.continent:
+    #         res = "%s \n %s" % (res, self.continent)
+    #     return res
