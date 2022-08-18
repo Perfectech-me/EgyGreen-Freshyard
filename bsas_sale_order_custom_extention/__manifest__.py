5e86396d33d8dc8c746a5cecc19c0921721d2d9f
mohamed_salah123 <mohamedsalahkamel123@yahoo.com>
2022-08-18 15:10:07 +0200

    work in sale

--- bsas_sale_order_custom_extention/__manifest__.py
+++ bsas_sale_order_custom_extention/__manifest__.py
@@ -3,7 +3,7 @@
     'name': "Sale Order Customize Extension",
     'category': 'Sales',
     'version': '15.0',
-    'depends': ['base', 'bsas_sale_order_custom'],
+    'depends': ['base', 'account','sale','account_accountant'],
 
     'data': [
         # 'security/ir.model.access.csv',
