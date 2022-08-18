a6df239bccf3cf13fd50f76c04e93dd4ff3e43a2
mohamed_salah123 <mohamedsalahkamel123@yahoo.com>
2022-08-18 15:19:39 +0200

    work in sales

---
+++ bsas_sale_order_custom_extention/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sale Order Customize Extension",
+    'category': 'Sales',
+    'version': '15.0',
+    'depends': ['base', 'account','sale','account_accountant'],
+
+    'data': [
+        'security/ir.model.access.csv',
+        'views/customer_type.xml',
+        'views/res_partner.xml',
+    ],
+    'license': 'LGPL-3',
+
+}
