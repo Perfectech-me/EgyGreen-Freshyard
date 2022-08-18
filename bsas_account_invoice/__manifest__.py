5baacfdd6a1d8e8e1fd703a4153d771658ccd31b
mohamed_salah123 <mohamedsalahkamel123@yahoo.com>
2022-08-18 14:27:17 +0200

    work in invoice

--- bsas_account_invoice/__manifest__.py
+++ bsas_account_invoice/__manifest__.py
@@ -4,11 +4,13 @@
 
     'category': 'Accounting',
     'version': '15.0',
-    'depends': ['base','account','account_accountant'],
+    'depends': ['base','account','account_accountant','purchase_extra'],
     'data': [
-        # 'security/ir.model.access.csv',
         'views/account_move.xml',
+        'views/partner.xml',
+        'views/taxes.xml',
         'report/invoice_report.xml',
+        # 'report/receipt_discount_tax_report.xml',
     ],
     'license': 'LGPL-3',
 
