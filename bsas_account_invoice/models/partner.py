310609c48904b80dddec259fccc9c8f4c929b847
mohamed_salah123 <mohamedsalahkamel123@yahoo.com>
2022-08-18 16:24:00 +0200

    remove report

--- bsas_account_invoice/models/partner.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api
-class ResPartnerInherit(models.Model):
-    _inherit = 'res.partner'
-
-    tax_commission_file_no = fields.Char(string="Tax Commission File No")
-
