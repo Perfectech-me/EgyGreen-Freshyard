d693811e2169600a7d4271635c405a53aa2e2755
mohamed_salah123 <mohamedsalahkamel123@yahoo.com>
2022-08-18 15:16:23 +0200

    work in sales

---
+++ bsas_sale_order_custom/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from . import sale_order
+from . import port_loading
+from . import partner
+from . import purchase_order
+from . import shipment_line
+from . import sales_person_users
+from . import account_move
---
+++ bsas_sale_order_custom/wizard/__init__.py
@@ -0,0 +1,3 @@
+
+from . import approve_credit_limit
+from . import refuse_credit_limit
