8025fec16dfbb1b375c81d1390556eed6608722e
mohamed_salah123 <mohamedsalahkamel123@yahoo.com>
2022-09-05 18:41:24 +0200

    Update account Checks

--- account_cheque/__manifest__.py
+++ account_cheque/__manifest__.py
@@ -1,45 +1,24 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "account_cheque",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'name': "Accounting checks",
 
     'description': """
-        Long description of module's purpose
+        Accounting Checks
     """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'category': 'Accounting',
+    'version': '15.1',
     'depends': ['base', 'account'],
-
-    # always loaded
     'data': [
         'security/cheque_security.xml',
         'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
         'views/cashed_outgoing_wizard.xml',
-        'views/incoming_report.xml',
-        'views/outgoing_report.xml',
-        'views/print_cheque.xml',
-        'views/cheque_print.xml',
+        'reports/incoming_report.xml',
+        'reports/outgoing_report.xml',
+        'reports/print_cheque.xml',
+        'reports/cheque_print.xml',
+        'reports/epe_check.xml',
+    ],
 
-        # 'views/security_encryption.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
-    'license': 'OEEL-1',
-    # 'installable': True,
+    'license': 'LGPL-3',
 }
