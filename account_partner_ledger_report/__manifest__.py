46d5b04e5f0aeed250e3febe7249ce2836e5cc7f
mohamed_salah123 <mohamedsalahkamel123@yahoo.com>
2022-10-02 17:43:34 +0200

    work in reports

--- account_partner_ledger_report/__manifest__.py
+++ account_partner_ledger_report/__manifest__.py
@@ -15,17 +15,17 @@
     # always loaded
     'data': [
             'security/ir.model.access.csv',
-            'views/search_template_view.xml',
+            # 'views/search_template_view.xml',
             'wizard/partner_ledger_custom_report.xml',
             'reports/partner_ledger_report.xml',
 
     ],
     'license': 'LGPL-3',
 
-    'assets': {
-        'web.assets_backend': [
-            'account_partner_ledger_report/static/src/js/custom_account_reports.js'
-        ],
-    }
+    # 'assets': {
+    #     'web.assets_backend': [
+    #         'account_partner_ledger_report/static/src/js/custom_account_reports.js'
+    #     ],
+    # }
 
 }
