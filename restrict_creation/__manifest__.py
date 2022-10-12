9a960c7bcd9011256661a8c97dbc8947bc104f1a
AhmedEissa2021 <88287282+AhmedEissa2021@users.noreply.github.com>
2022-10-12 14:48:33 +0200

    Add files via upload
    Signed-off-by: AhmedEissa2021 <88287282+AhmedEissa2021@users.noreply.github.com>

---
+++ restrict_creation/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Restrict Creation",
+
+    'description': """
+        Restrict Creation
+    """,
+    'category': 'Inventory/Inventory',
+    'version': '15.1',
+    'depends': ['base','delivery', 'product','account','sale_stock', 'sale_management','stock'],
+    'data': [
+        'security/security.xml',
+
+
+    ],
+
+    'license': 'LGPL-3',
+}
