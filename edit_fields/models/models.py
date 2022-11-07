6ebda55485887e9e4d37e9241f0be1d11837d083
husseinmagdy7 <111385117+husseinmagdy7@users.noreply.github.com>
2022-11-07 15:50:44 +0200

    Add files via upload
    Signed-off-by: husseinmagdy7 <111385117+husseinmagdy7@users.noreply.github.com>

---
+++ edit_fields/models/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import base64
+
+from num2words import num2words
+
+from odoo import models, fields, api, _,tools
+from odoo.exceptions import UserError, ValidationError
+from datetime import datetime, timedelta
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+    company_id = fields.Many2one('res.company', 'Company', index=True,required=True)
+
+
+    # comp
