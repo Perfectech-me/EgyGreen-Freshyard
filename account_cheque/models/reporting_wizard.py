ae3bbce823befcf3c54d5d5adc04236f59109147
toolsmohamedsalahkamel <you@example.com>
2022-12-13 16:57:48 +0200

    work in cheque report

--- account_cheque/models/reporting_wizard.py
+++ account_cheque/models/reporting_wizard.py
@@ -9,6 +9,10 @@ class account_move_report_wizard(models.Model):
     payee_user_id = fields.Many2one(comodel_name="res.partner", string="Payee", required=False, )
     from_date = fields.Date(string="From", required=True, )
     to_date = fields.Date(string="To", required=True, )
+
+
+    given_from_date = fields.Date(string="From")
+    given_to_date = fields.Date(string="To")
     status = fields.Selection(string="",
                               selection=[('draft', 'Draft'), ('registered', 'Registered'), ('bank', 'Bank Repository'),
                                          ('bounced', 'Bounced'),
@@ -17,7 +21,16 @@ class account_move_report_wizard(models.Model):
                             required=True )
 
     analytic_account_id = fields.Many2one(comodel_name="account.analytic.account", string="Analytic Account")
+    bank_account_id = fields.Many2one(comodel_name="account.account", string="Bank Account")
+    account_type_ids = fields.Many2many(comodel_name="account.account.type", string="نوع الحساب" ,compute='compute_account_type')
 
+    @api.depends('from_date', 'to_date')
+    def compute_account_type(self):
+        for rec in self:
+            rec.account_type_ids = False
+            lines = []
+            lines.append(self.env.ref('account.data_account_type_liquidity').id)
+            rec.account_type_ids = lines
 
     # @api.multi
     def get_today(self):
@@ -30,10 +43,15 @@ class account_move_report_wizard(models.Model):
     # @api.multi
     def generate_cheque_report(self):
         domain=[]
-        if self.from_date > self.to_date:
-            raise ValidationError('تاريخ البدأ أكبر من تاريخ النتهاء')
+
+
         domain.append (('cheque_date', '>=', self.from_date))
         domain.append(('cheque_date', '<=', self.to_date))
+
+        if self.given_from_date and self.given_to_date:
+            domain.append(('cheque_given_date', '>=', self.from_date))
+            domain.append(('cheque_given_date', '<=', self.to_date))
+
         if self.payer_user_id:
             domain.append(('payer_user_id', '=', self.payer_user_id.id))
         if self.status:
@@ -43,6 +61,13 @@ class account_move_report_wizard(models.Model):
 
         if self.analytic_account_id:
             domain.append(('analytic_account_id', '=', self.analytic_account_id.id))
+
+        if self.analytic_account_id:
+            domain.append(('analytic_account_id', '=', self.analytic_account_id.id))
+
+        if self.bank_account_id:
+            domain.append(('bank_account_id', '=', self.bank_account_id.id))
+
 
         domain.append(('type', '=', self.type))
         print(domain)
@@ -54,6 +79,17 @@ class account_move_report_wizard(models.Model):
 
     # @api.multi
     def outgoing_report_method(self):
+        if (self.given_from_date and not self.given_to_date) or (self.given_to_date and not self.given_from_date):
+            raise ValidationError('الرجاء ادخال التواريخ بشكل صحيح من والى')
+
+        if self.given_from_date and self.given_to_date and self.given_from_date > self.given_to_date :
+            raise ValidationError('تاريخ البدأ أكبر من تاريخ النتهاء')
+
+
+        if self.from_date > self.to_date :
+            raise ValidationError('تاريخ البدأ أكبر من تاريخ النتهاء')
+
+
         return self.env.ref('account_cheque.outgoing_report_get').report_action(self)
 
     def outgoing_print_method(self):
