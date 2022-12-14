197615eec4c11a4f058ff9838c64e769854c0d5d
toolsmohamedsalahkamel <you@example.com>
2022-12-14 10:04:49 +0200

    edit in some reports

--- egygreen_sale_order_reports/models/sale_order.py
+++ egygreen_sale_order_reports/models/sale_order.py
@@ -4,10 +4,14 @@ class SaleOrderLine(models.Model):
 
     net_weight_per_unit = fields.Float(string="Net Weight Per Unit")
     gross_weight_per_unit = fields.Float(string="Gross Weight Per Unit")
+    container_equipment_number = fields.Char(string="Container Equipment Number")
+
+
 class SaleOrderInherit(models.Model):
     _inherit = 'sale.order'
 
     partner_bank_id = fields.Many2one(comodel_name="res.partner.bank", string="Recipient Bank")
+    data_loger = fields.Text(string="Data Loger")
 
 
 class SaleAdvancePaymentInvInherit(models.TransientModel):
