2d4ccf9d69bb5cf8fa51ee27025a04d84afe7ec2
mohamedsalem99 <mohamed.salem8843@gmail.com>
2023-07-30 11:20:02 +0300

    T4097

---
+++ pr_payment_terms_customization/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Payment Terms Customization',
+    'version': '15',
+    'summary': 'Payment Terms Customization',
+    'author': 'Perfect-tech - Mohamed Salem',
+    'license': 'LGPL-3',
+    'depends': ['sale', 'account', 'base', 'product', 'analytic'],
+    'data': [
+        'views/payment_terms_view.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
