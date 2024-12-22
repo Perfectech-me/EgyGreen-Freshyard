165951e8a7f380de20203f993e1fa09794a00c7e
Nada-Soliman <nada.mohammad7654@gmail.com>
2024-12-22 12:26:07 +0200

    Task0997-landed cost

--- inventory_customization/models/stock_landed_cost.py
+++ inventory_customization/models/stock_landed_cost.py
@@ -30,28 +30,42 @@ class StockLandedCostInherit(models.Model):
     )
     mrp_production_ids = fields.Many2many(
         'mrp.production', string='Manufacturing order',
-        copy=False, states={'done': [('readonly', True)]}, groups='stock.group_stock_manager')
+        copy=False, states={'done': [('readonly', True)]},
+        compute='_compute_mrp_production_ids',
+        groups='stock.group_stock_manager')
+
+    @api.depends('date', 'date_to', 'company_id')
+    def _compute_mrp_production_ids(self):
+        for record in self:
+            mrp_production_values = self.env['mrp.production']
+            if record.date and record.date_to:
+                mrp_production_values = mrp_production_values.search([
+                    ('date_planned_start', '>=', record.date),
+                    ('date_planned_start', '<=', record.date_to),
+                    ('state', '=', 'done')
+                ])
+            if record.company_id:
+                mrp_production_values = mrp_production_values.filtered(
+                    lambda production: production.company_id.id == record.company_id.id
+                )
+            record.mrp_production_ids = mrp_production_values
+            print("record.mrp_production_ids",record.mrp_production_ids)
 
     @api.onchange('date', 'date_to', 'company_id')
     def _onchange_date_range(self):
         domain_picking = []
-        domain_mrp = []
+        mrp_production_values = self.env['mrp.production']
 
         if self.date and self.date_to:
             domain_picking.append(('scheduled_date', '>=', self.date))
             domain_picking.append(('scheduled_date', '<=', self.date_to))
 
-            domain_mrp.append(('date_planned_start', '>=', self.date))
-            domain_mrp.append(('date_planned_start', '<=', self.date_to))
 
         if self.company_id:
             domain_picking.append(('company_id', '=', self.company_id.id))
-            domain_mrp.append(('company_id', '=', self.company_id.id))
-        domain_mrp.append(('state', '=', 'done'))
 
         return {
             'domain': {
                 'picking_ids': domain_picking,
-                'mrp_production_ids': domain_mrp,
             }
         }
