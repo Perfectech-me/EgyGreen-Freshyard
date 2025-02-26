24351435f48024d6e848a803d4dd3349b8a3790b
Nada-Soliman <nada.mohammad7654@gmail.com>
2025-02-26 11:23:47 +0200

    Task2056

---
+++ account_cheque_report_edit/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "report edit",
+    'category': 'Accounting',
+    'version': '15.0',
+    'depends': ['base','account','sale','sale_stock','account_accountant'],
+
+    'data': [
+        'reports/cib_cheque_edit.xml',
+
+    ],
+    'license': 'LGPL-3',
+
+}
