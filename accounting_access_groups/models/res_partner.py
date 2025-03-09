0868d38e964f722bde6c47156e367ca4d1746dfb
Nada-Soliman <nada.mohammad7654@gmail.com>
2025-03-09 09:33:31 +0200

    Task2199-in vendor and customer the crate is in a group

---
+++ accounting_access_groups/models/res_partner.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api, exceptions
+
+
+
+class ResPartnerInherit(models.Model):
+    _inherit = 'res.partner'
+
+
+    @api.model
+    def create(self, vals):
+        if not self.env.user.has_group('accounting_access_groups.group_partner_create'):
+            raise exceptions.ValidationError("You do not have the permission to create partner.")
+        return super(ResPartnerInherit, self).create(vals)
