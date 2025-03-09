7da3aa5530609a1706b14cea8668d2b68956c600
Nada-Soliman <nada.mohammad7654@gmail.com>
2025-03-09 09:49:38 +0200

    Task2199-in vendor and customer the crate is in a group

---
+++ accounting_access_groups/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Accounting Access Groups",
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+    'description': """
+        Long description of module's purpose
+    """,
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base','account'],
+
+    # always loaded
+    'data': [
+        'groups/security.xml'
+    ],
+}
