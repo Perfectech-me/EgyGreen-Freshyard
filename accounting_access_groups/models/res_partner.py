a097d59b587e3426c2dbdff9a409470cb84805d7
Nada-Soliman <nada.mohammad7654@gmail.com>
2025-03-16 10:05:34 +0200

    Task2199-in vendor and customer the edit is in a group

--- accounting_access_groups/models/res_partner.py
+++ accounting_access_groups/models/res_partner.py
@@ -12,3 +12,10 @@ class ResPartnerInherit(models.Model):
         if not self.env.user.has_group('accounting_access_groups.group_partner_create'):
             raise exceptions.ValidationError("You do not have the permission to create partner.")
         return super(ResPartnerInherit, self).create(vals)
+
+    def write(self, vals):
+        if not self.env.user.has_group('accounting_access_groups.group_partner_edit'):
+            raise exceptions.ValidationError("You do not have the permission to edit partner.")
+        return super(ResPartnerInherit, self).write(vals)
+
+
