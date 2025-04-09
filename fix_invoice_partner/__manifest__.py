4cfda66ae5de19b3b403b6d8215bff3dd9223188
hebaibrahimahmed <hebaalhwaig96@gmail.com>
2025-04-09 15:15:45 +0200

    Task2314

---
+++ fix_invoice_partner/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "fix invoice partner",
+
+    'description': """
+        Edit field
+    """,
+    'category': 'Inventory/Inventory',
+    'version': '15.1',
+    'depends': ['base', 'sale'],
+    'data': [
+        'views/inherited_sale_order.xml',
+    ],
+
+    'license': 'LGPL-3',
+}
